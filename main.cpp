1376698b73b764b676b05a77e04bd3df7ab75010
aralsto <alexralston38@gmail.com>
2020-02-03 10:08:31 -0500

    scraggy

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 
-void init(){
-    
+void init() {
+    float pos[3];
+    pos[0] = 0.0;
+    pos[1] = 0.0;
+    pos[2] = 1.0;
 }
 
-void loop(){
-    
+void loop() {
+    api.setPositionTarget(pos);
 }
