35e3af7a604d20147fe3b996c8cc7ad4ac5e3ed8
Avery Newman <jonah.h.newman@gmail.com>
2020-02-19 09:50:48 -0500

    merge commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-initial text
-second new text
-doggy
-zdfsgf
 etretseft
 wertertsadfg
 nnew text
+nnnnew text 
 nnnnew text
 merge condition testing
+
+jonah merge edits
+for testing
 this should conflict with changes from the extension if they happen at the same time
