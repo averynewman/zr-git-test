5a191025f7279de4e1664384268c06f4d5e8f218
Avery Newman <jonah.h.newman@gmail.com>
2020-02-19 10:35:42 -0500

    reset branch contents

--- main.cpp
+++ main.cpp
@@ -1,10 +1,1 @@
-etretseft
-wertertsadfg
-nnew text
-nnnnew text 
-nnnnew text
-merge condition testing
-
-jonah merge edits
-for testing
-this should conflict with changes from the extension if they happen at the same time
+reset branch contents
